3d1ed44f12db30bd6b438c49767c1f9b31c7960d
Carlos Eduardo Varela de Almeida <caduvarela@gmail.com>
2021-09-22 16:20:07 -0300

    Ble

---
+++ js/iniciaTela.js
@@ -0,0 +1,6 @@
+function iniciaGrade() {
+    //document.getElementById('tela-jogo').style = 'background-color: red;';
+    let tela = document.getElementById('tela-jogo');
+    let HTMLgrade = '<h1>Olá</h1>';
+    tela.insertAdjacentHTML('afterbegin', HTMLgrade);
+}
