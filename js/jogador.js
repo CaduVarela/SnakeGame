03b84b128101c44b86561f73e4e4bb348ea04c82
Carlos Eduardo Varela de Almeida <caduvarela@gmail.com>
2021-10-01 14:08:56 -0300

    Update jogador.js

--- js/jogador.js
+++ js/jogador.js
@@ -8,48 +8,53 @@ var posicaoJogador = {}
 posicaoJogador.x = 15;
 posicaoJogador.y = 15;
 
+var idIntervaloMovimento;
 var historicoMovimentos = [];
 var gradeAtual; // Elemento DOM em que 'snake-head' se encontra
 
 function controlarJogador(tecla) {
     tamanhoGrade = document.querySelectorAll('#tela-jogo tbody tr').length;
-    if (posicaoJogador.x == -1 || posicaoJogador.x == tamanhoGrade || posicaoJogador.y == -1 || posicaoJogador.y == tamanhoGrade) {
-        console.log('perdeu');
-        clearInterval(idIntervalo)
-        return;
-    }
 
     //console.log(tamanhoGrade)
     //console.log(tecla)
 
     // Inicia movimento automatico de COBRA
     if (historicoMovimentos.length == 0) {
-        var idIntervalo = setInterval(manterMovimentoJogador, 80);
+        idIntervaloMovimento = setInterval(manterMovimentoJogador, 80);
     }
 
     getGradeAtual().classList.remove('snake-head');
     switch (tecla) {
         case 'w':
         case 'ArrowUp':
-            atualizarHistorico('cima');
+            if (historicoMovimentos[historicoMovimentos.length-1] != 'baixo') {
+                atualizarHistorico('cima');
+            }
             //posicaoJogador.y--;
             break;
         case 'a':
         case 'ArrowLeft':
-            atualizarHistorico('esquerda');
+            if (historicoMovimentos[historicoMovimentos.length-1] != 'direita') {
+                atualizarHistorico('esquerda');
+            }
             //posicaoJogador.x--;
             break;
         case 's':
         case 'ArrowDown':
-            atualizarHistorico('baixo');
+            if (historicoMovimentos[historicoMovimentos.length-1] != 'cima') {
+                atualizarHistorico('baixo');
+            }
             //posicaoJogador.y++;
             break;
         case 'd':
         case 'ArrowRight':
-            atualizarHistorico('direita');
+            if (historicoMovimentos[historicoMovimentos.length-1] != 'esquerda') {
+                atualizarHistorico('direita');
+            }
             //posicaoJogador.x++;
             break;
     }
+
     posicionarJogador();
 }
 
@@ -60,6 +65,7 @@ function manterMovimentoJogador() {
     if (historicoMovimentos.length == 0) {
         return
     }
+
     switch (historicoMovimentos[historicoMovimentos.length-1]) {
         case 'cima':
             posicaoJogador.y--;
@@ -74,11 +80,27 @@ function manterMovimentoJogador() {
             posicaoJogador.x++;
             break;
     }
+    //console.log(tamanhoGrade);
+    //console.log(`(${posicaoJogador.x}, ${posicaoJogador.y})`);
+    
+    if (verificarDerrota()) {
+        console.log('perdeu');
+        return;
+    }
     posicionarJogador();
 }
 
 function posicionarJogador() {
     getGradeAtual().classList.add('snake-head');
+}
+
+function verificarDerrota() {
+    if (posicaoJogador.x <= -1 || posicaoJogador.x >= tamanhoGrade || posicaoJogador.y <= -1 || posicaoJogador.y >= tamanhoGrade) {
+        //console.log('perdeu');
+        clearInterval(idIntervaloMovimento);
+        return true;
+    }
+    return false;
 }
 
 function crescerJogador() {
@@ -96,8 +118,6 @@ function crescerJogador() {
             break;
         case 'direita':
             getGrade(posicaoJogador.x-1, posicaoJogador.y).classList.add('snake-body');
-            break;
-        default:
             break;
     }
 }
